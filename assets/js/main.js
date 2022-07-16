673266f5eb3a7a618dd0e448578351c83c28c02e
lnam6507 <lnam6507@gmail.com>
2022-07-16 11:43:17 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,32 +3,39 @@ const songs = [
     id: 1,
     name: "Cơn Mưa Ngang Qua",
     author: "Sơn Tùng MTP",
-    avatar: "./assets/images/cmnq.jpg",
+    avatar: "./assets/images/cmnq.png",
     path: "./assets/song/cmnq.mp3",
   },
   {
     id: 2,
+    name: "Em Của Ngày Hôm Qua - Onionn Remix",
+    author: "Sơn Tùng MTP",
+    avatar: "./assets/images/ecnhq.png",
+    path: "./assets/song/ecnhq.mp3",
+  },
+  {
+    id: 3,
     name: "Có chắc yêu là đây - Onionn Remix",
     author: "Sơn Tùng MTP",
     avatar: "./assets/images/ccyld.jpg",
     path: "./assets/song/ccyld.mp3",
   },
   {
-    id: 3,
+    id: 4,
     name: "Có chắc yêu là đây",
     author: "Sơn Tùng MTP",
     avatar: "./assets/images/ccyld1.jpg",
     path: "./assets/song/ccyld1.m4a",
   },
   {
-    id: 4,
+    id: 5,
     name: "Muộn rồi mà sao còn",
     author: "Sơn Tùng MTP",
     avatar: "./assets/images/mrmsc.jpg",
     path: "./assets/song/mrmsc.mp3",
   },
   {
-    id: 5,
+    id: 6,
     name: "Chúng ta của hiện tại",
     author: "Sơn Tùng MTP",
     avatar: "./assets/images/ctcht.jpg",
@@ -37,10 +44,13 @@ const songs = [
 ];
 
 let isPlay = true;
-let firstSong = songs[0];
 let indexSong = 0;
+let firstSong = songs[indexSong];
 
 let btnPlay = document.querySelector(".play-btn i");
+let btnPrev = document.querySelector(".prev-btn i");
+let btnNext = document.querySelector(".next-btn i");
+
 let songAvatar = document.querySelector(".song-avatar");
 let songName = document.querySelector(".song-info h3");
 let songAuthor = document.querySelector(".song-info h5");
@@ -49,10 +59,11 @@ let audio = document.querySelector(".audio");
 let lists = document.querySelector(".lists");
 
 audio.setAttribute("src", firstSong.path);
-songName.innerHTML = firstSong.name
-songAuthor.innerHTML = firstSong.author
+songName.innerHTML = firstSong.name;
+songAuthor.innerHTML = firstSong.author;
 urlAvatar.setAttribute("src", firstSong.avatar);
 
+// Render HTML
 songs.forEach((e) => {
   var div = document.createElement("div");
   var html = `
@@ -69,8 +80,10 @@ songs.forEach((e) => {
   div.innerHTML = html;
   lists.appendChild(div);
 });
-// onclick="play('${e.name}','${e.author}','${e.path}','${e.avatar}','${e.id}')" key="${e.id}"
+
+
 const play = (name, author, path, avatar, key) => {
+  indexSong = key - 1;
   audio.setAttribute("src", path);
   audio.play();
   urlAvatar.setAttribute("src", avatar);
@@ -100,4 +113,46 @@ btnPlay.onclick = () => {
     songAvatar.classList.add("pause");
     isPlay = true;
   }
+};
+
+btnPrev.onclick = () => {
+  indexSong = indexSong - 1
+  if(indexSong<0){
+    indexSong = songs.length - 1
+  }
+  console.log(indexSong);
+  audio.setAttribute("src", songs[indexSong].path);
+  audio.play();
+  urlAvatar.setAttribute("src", songs[indexSong].avatar);
+  songName.innerHTML = songs[indexSong].name;
+  songAuthor.innerHTML = songs[indexSong].author;
+  btnPlay.setAttribute("class", "fas fa-pause");
+  songAvatar.classList.remove("pause");
+  songAvatar.classList.add("play");
+  var list_item = document.querySelectorAll(".lists-item");
+  list_item.forEach((e) => {
+    e.getAttribute("key") == songs[indexSong].id
+      ? e.classList.add("active")
+      : e.classList.remove("active");
+  });
+};
+btnNext.onclick = () => {
+  indexSong += 1;
+  if (indexSong >= songs.length) {
+    indexSong = 0;
+  }
+  audio.setAttribute("src", songs[indexSong].path);
+  audio.play();
+  urlAvatar.setAttribute("src", songs[indexSong].avatar);
+  songName.innerHTML = songs[indexSong].name;
+  songAuthor.innerHTML = songs[indexSong].author;
+  btnPlay.setAttribute("class", "fas fa-pause");
+  songAvatar.classList.remove("pause");
+  songAvatar.classList.add("play");
+  var list_item = document.querySelectorAll(".lists-item");
+  list_item.forEach((e) => {
+    e.getAttribute("key") == songs[indexSong].id
+      ? e.classList.add("active")
+      : e.classList.remove("active");
+  });
 };
